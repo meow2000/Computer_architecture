c7826c739bab6813f4f740b02cfa86c88c9729de
mlem <33568957+meow2000@users.noreply.github.com>
2021-05-27 05:22:54 +0700

    finish testing IsDigit

--- Funct.asm
+++ Funct.asm
@@ -3,22 +3,23 @@ input: .space 100 # Buffer 100 byte chua chuoi ki tu can
 .text
 ReadInput: # read infix
 # TO DO
- li $v0, 8
- la $a0, input
- li $a1, 100
+ li 	$v0, 8
+ la 	$a0, input
+ li 	$a1, 100
  syscall 
- la $t0, input
+ la 	$t0, input
+ jr 	$ra
 Push: # push in stack
 # TO DO
-
-Pop: # pop and save to $a0
+sub	$sp, $sp, 4	# add $sp
+sw	$a0, 0($sp)	# push
+Pop: # output : $a0
 # TO DO
 lw	$s0, ($sp)	# pop from stack
 addi	$sp, $sp, 4	# pop
 
 IsDigit: # input : $a0
-	 # output : $a0, $a1
-# TO DO 
+	 # output : $a0, $a1 
 addi	$t1, $zero, 48	# $t1 = '0'
 loop:
 addi	$t2, $zero, 57	# $t2 = '9'
@@ -36,9 +37,10 @@ yes:
 addi	$a0, $t1, -48	# $a0 = (int) token
 addi	$a1, $zero, 1	# yes 
 jr	$ra
+
 GetPrio: # get priority and save in $a0
 # TO DO
-
+addi	$t0, $zero, 43 
 In2post: # convert from inFix[] to postFix[]
 # TO DO
 
--- main.asm
+++ main.asm
@@ -1,11 +1,7 @@
-.data
-input: .space 100 # Buffer 100 byte chua chuoi ki tu can 
 .text
-ReadInput: # read infix
-# TO DO
- li $v0, 8
- la $a0, input
- li $a1, 100
- syscall 
- la $t0, input
-#.include "Funct.asm"
+jal	ReadInput
+lb	$a0, input
+jal	IsDigit
+li	$v0, 10	# terminate program run and
+syscall		# Exit 
+.include "Funct.asm"
