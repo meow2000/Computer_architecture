b1a36c3eb5a9033c9f498887d371d0dabd0e4961
mlem <33568957+meow2000@users.noreply.github.com>
2021-05-26 07:16:49 +0700

    push

--- Funct.asm
+++ Funct.asm
@@ -1,7 +1,22 @@
 .text
 readInput: # read infix
+# TO DO
+
 Push: # push in stack
+# TO DO
+
 Pop: # pop and save to $a0
+# TO DO
+
 IsDigit: # check if it is digit
-Precedence # check Precedence and save to $a0
-in2post # 
+# TO DO 
+
+GetPrio: # get priority and save in $a0
+# TO DO
+
+In2post: # convert from inFix[] to postFix[]
+# TO DO
+
+Calculate: # calculate 
+# TO DO
+
